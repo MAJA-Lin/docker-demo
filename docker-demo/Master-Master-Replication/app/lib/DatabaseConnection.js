5eb57ba35fa3bebb49b7602147d72867a1e039de
Scott Lin <MAJA-Lin@users.noreply.github.com>
2018-11-28 16:18:45 +0800

    Use promise to generate data

--- docker-demo/Master-Master-Replication/app/lib/DatabaseConnection.js
+++ docker-demo/Master-Master-Replication/app/lib/DatabaseConnection.js
@@ -24,6 +24,17 @@ class DatabaseConnection {
     close() {
         this.connection.end();
     }
+
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, function (error, results, fields) {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            });
+        });
+    }
 }
 
 module.exports = DatabaseConnection
--- docker-demo/Master-Master-Replication/app/script.js
+++ docker-demo/Master-Master-Replication/app/script.js
@@ -9,17 +9,29 @@ let secondMaster = new DatabaseConnection(secondMasterHost);
 
 firstMaster.connect();
 
-firstMaster.connection.query(
-    'INSERT INTO persons (last_name, middle_name, first_name) VALUES (?, ?, ?)',
-    [faker.name.lastName(), faker.name.title(), faker.name.firstName()],
-    function (error, results, fields) {
-        if (error) {
-            throw error;
-        }
+let tasks = []
 
-        console.log(results);
-    }
-)
+for (let i = 0; i < 5; i++) {
+    let firstPromise = firstMaster.query(
+        'INSERT INTO persons (last_name, middle_name, first_name) VALUES (?, ?, ?)',
+        [faker.name.lastName(), faker.name.title(), faker.name.firstName()],
+    );
+
+    let secondPromise = secondMaster.query(
+        'INSERT INTO persons (last_name, middle_name, first_name) VALUES (?, ?, ?)',
+        [faker.name.lastName(), faker.name.title(), faker.name.firstName()],
+    );
+
+    tasks.push(firstPromise);
+    tasks.push(secondPromise);
+}
+
+// Collect all finished promises
+Promise.all(tasks)
+    .then(function () {
+        console.log("Insert into master 1 completed.");
+    });
+
 
 
 firstMaster.close();
